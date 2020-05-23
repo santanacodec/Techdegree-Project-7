0103ee94000a68644a116d1772225f7708c86234
Edgar Santana <santanaprogrammer@gmail.com>
2020-05-23 12:49:19 -0400

    added charts
    added bar and doughnut charts

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// line chart
 var ctx = document.getElementById('myChart');
 var myChart = new Chart(ctx, {
     type: 'line',
@@ -71,7 +72,7 @@ var myChart = new Chart(ctx, {
         },
             {
             label: 'Weekly',
-            data: [0, 500, 1000, 750, 1250, 1750, 1250, 1500, 2000, 1500, 2000],
+            data: [0, 500, 1000, 750, 1250, 1750, 1250, 1500, 2000, 1500, 2000, 2500],
             backgroundColor: [
                 'rgba(226, 227, 246, 0.7)',
                 'rgba(251, 251, 251)',
@@ -140,6 +141,94 @@ var myChart = new Chart(ctx, {
         title: {
             display: true,
             text: 'TRAFFIC'
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+// Bar Chart
+var barChart = document.getElementById('myBarChart').getContext('2d');
+var myChart = new Chart(barChart, {
+    type: 'bar',
+    data: {
+        labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+        datasets: [{
+            label: '# of Votes',
+            data: [50, 75, 150, 100, 200, 175, 75, 250],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        title: {
+            display: true,
+            text: 'DAILY TRAFFIC'
+        },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
+
+// Doughnut Chart
+var doughnutChart = document.getElementById('myDoughnutChart').getContext('2d');
+var myChart = new Chart(doughnutChart, {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        title: {
+            display: true,
+            text: 'MOBILE USERS'
         },
         scales: {
             yAxes: [{
